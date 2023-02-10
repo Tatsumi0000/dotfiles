bad627ebdbafff3a61fac769e0a1feb038d3b8af
Tatsumi0000 <datsunote0211@gmail.com>
2023-02-11 05:17:43 +0900

    ファイル名変えた

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CURRENT_DIR=$(cd $(dirname $0) && pwd)
+ln -sf ${CURRENT_DIR}/nvim/init.lua ~/.config/nvim/init.lua
+ln -sf ${CURRENT_DIR}/nvim/lua/plugins/appearance.lua ~/.config/nvim/lua/plugins/appearance.lua
+ln -sf ${CURRENT_DIR}/nvim/lua/plugins/fern.lua ~/.config/nvim/lua/plugins/fern.lua
+ln -sf ${CURRENT_DIR}/nvim/lua/plugins/telescope.lua ~/.config/nvim/lua/plugins/telescope.lua
+ln -sf ${CURRENT_DIR}/fish/config.fish ~/.config/fish/config.fish
+

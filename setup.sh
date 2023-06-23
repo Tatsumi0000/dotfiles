8c5607aedcecdc341a3c270d6b25e569c53e1dc5
Tatsumi0000 <datsunote0211@gmail.com>
2023-06-23 11:56:14 +0900

    tmuxの設定を入れた

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ ln -sf ${CURRENT_DIR}/nvim/lua/plugins/appearance.lua ~/.config/nvim/lua/plugins
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/fern.lua ~/.config/nvim/lua/plugins/fern.lua
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/telescope.lua ~/.config/nvim/lua/plugins/telescope.lua
 ln -sf ${CURRENT_DIR}/fish/config.fish ~/.config/fish/config.fish
-
+ln -sf ${CURRENT_DIR}/tmux/tmux.conf ~/tmux.conf

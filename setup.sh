acb611109cfcc90345b7d45c83a6a076be5dde78
Tatsumi0000 <datsunote0211@gmail.com>
2023-07-04 19:25:14 +0900

    gitsignsの設定を入れた

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@ ln -sf ${CURRENT_DIR}/nvim/init.lua ~/.config/nvim/init.lua
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/appearance.lua ~/.config/nvim/lua/plugins/appearance.lua
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/fern.lua ~/.config/nvim/lua/plugins/fern.lua
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/telescope.lua ~/.config/nvim/lua/plugins/telescope.lua
+ln -sf ${CURRENT_DIR}/nvim/lua/plugins/gitsigns.lua ~/.config/nvim/lua/plugins/gitsigns.lua
 ln -sf ${CURRENT_DIR}/fish/config.fish ~/.config/fish/config.fish
 ln -sf ${CURRENT_DIR}/tmux/tmux.conf ~/tmux.conf

8298868ce3686b439b42c1d74be31b5664659551
Tatsumi0000 <datsunote0211@gmail.com>
2023-07-05 21:30:08 +0900

    tmuxの設定も管理する

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ ln -sf ${CURRENT_DIR}/nvim/lua/plugins/fern.lua ~/.config/nvim/lua/plugins/fern.
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/telescope.lua ~/.config/nvim/lua/plugins/telescope.lua
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/gitsigns.lua ~/.config/nvim/lua/plugins/gitsigns.lua
 ln -sf ${CURRENT_DIR}/fish/config.fish ~/.config/fish/config.fish
-ln -sf ${CURRENT_DIR}/tmux/tmux.conf ~/tmux.conf
+ln -sf ${CURRENT_DIR}/tmux/.tmux.conf ~/.tmux.conf

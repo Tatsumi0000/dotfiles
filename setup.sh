36f2a29a9cd29af16e931c4c81e757214d7f5612
Tatsumi0000 <datsunote0211@gmail.com>
2023-10-09 00:07:05 +0900

    zshとgitの設定を追加

--- setup.sh
+++ setup.sh
@@ -7,6 +7,8 @@ ln -sf ${CURRENT_DIR}/nvim/lua/plugins/fern.lua ~/.config/nvim/lua/plugins/fern.
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/telescope.lua ~/.config/nvim/lua/plugins/telescope.lua
 ln -sf ${CURRENT_DIR}/nvim/lua/plugins/gitsigns.lua ~/.config/nvim/lua/plugins/gitsigns.lua
 ln -sf ${CURRENT_DIR}/fish/config.fish ~/.config/fish/config.fish
+ln -sf ${CURRENT_DIR}/zsh/.zshrc ~/.zshrc
 ln -sf ${CURRENT_DIR}/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ${CURRENT_DIR}/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 ln -sf ${CURRENT_DIR}/git/ignore ~/.config/git/ignore
+ln -sf ${CURRENT_DIR}/git/config ~/.config/git/config

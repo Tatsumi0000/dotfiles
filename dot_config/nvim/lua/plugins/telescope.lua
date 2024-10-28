e83b07871e92911731a51a72992af7cbe6062a5f
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2024-10-28 23:01:45 +0900

    とりあえずtelescopeを入れた

---
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,12 @@
+return {
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local set = vim.keymap.set
+		set("n", "<leader>ff", "<cmd>Telescope find_files<cr>",  { desc = "Telescope find_files" })
+		set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Telescope live_grep" })
+		set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Telescope buffers" })
+		set("n", "<leader>fif", require("telescope.builtin").git_files, { desc = "Fzf Files" })
+	end,
+}

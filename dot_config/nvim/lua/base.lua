9288ed39bd732cdb9cbd00ae56659c24bd063b40
Tatsumi0000 <19218690+Tatsumi0000@users.noreply.github.com>
2025-04-30 00:18:53 +0900

    Fix: yank時のハイライト時間

--- dot_config/nvim/lua/base.lua
+++ dot_config/nvim/lua/base.lua
@@ -54,6 +54,6 @@ vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#00DCFF" })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 800 })
+		vim.highlight.on_yank({ higroup = "YankHighlight", timeout = 300 })
 	end,
 })
